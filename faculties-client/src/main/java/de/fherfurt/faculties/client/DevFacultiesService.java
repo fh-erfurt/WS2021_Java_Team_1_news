03a89134a87930d6142f0425aa362b6e91f3127a
ch0914se <christof.seelisch@fh-erfurt.de>
2022-03-15 12:43:14 +0100

    FacultiesService adjusted to only check if a faculty exists

--- faculties-client/src/main/java/de/fherfurt/faculties/client/DevFacultiesService.java
+++ faculties-client/src/main/java/de/fherfurt/faculties/client/DevFacultiesService.java
@@ -12,19 +12,19 @@ import java.util.Optional;
 public class DevFacultiesService implements FacultiesService {
 
     private final List<NewsFaculty> faculties = Arrays.asList(
-            new NewsFaculty(1, "faculty1"),
-            new NewsFaculty(2, "faculty2"),
-            new NewsFaculty(3, "faculty3"),
-            new NewsFaculty(4, "faculty4")
+            new NewsFaculty("faculty1"),
+            new NewsFaculty("faculty2"),
+            new NewsFaculty("faculty3"),
+            new NewsFaculty("faculty4")
     );
 
     @Override
-    public Optional<NewsFaculty> isFacultynameValid(String name){
+    public boolean isFacultynameValid(String name){
         for (int i=0; i < 4; i++){
             if(faculties.get(i).getName().equals(name)){
-                return Optional.ofNullable(faculties.get(i));
+                return true;
             }
         }
-        return Optional.empty();
+        return false;
     }
 }
--- faculties-client/src/main/java/de/fherfurt/faculties/client/FacultiesService.java
+++ faculties-client/src/main/java/de/fherfurt/faculties/client/FacultiesService.java
@@ -2,12 +2,10 @@ package de.fherfurt.faculties.client;
 
 import de.fherfurt.faculties.client.transfer.objects.NewsFaculty;
 
-import java.util.Optional;
-
 /**
  * @author Christof Seelisch <christof.seelisch@fh-erfurt.de>
  */
 public interface FacultiesService {
 
-    Optional<NewsFaculty> isFacultynameValid(String name);
+    boolean isFacultynameValid(String name);
 }
