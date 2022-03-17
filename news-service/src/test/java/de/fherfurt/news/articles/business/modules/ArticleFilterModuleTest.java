f1ffda4f33fdcdbf6139fe77504d56d90710395a
MaximilianRoehr <maximilian.roehr@fh-erfurt.de>
2022-03-17 11:23:24 +0100

    added this for clearer code

---
+++ news-service/src/test/java/de/fherfurt/news/articles/business/modules/ArticleFilterModuleTest.java
@@ -0,0 +1,49 @@
+package de.fherfurt.news.articles.business.modules;
+
+import de.fherfurt.news.articles.entity.Article;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+class ArticleFilterModuleTest {
+
+    private final String ai = "Angewandte Informatik";
+    private final String ge = "Gebäude und Energie Technik";
+    private String facultyFilterName;
+
+    private final Article articleAI = Article.builder().withFacultyName(ai).build();
+    private final Article articleAI2 = Article.builder().withFacultyName(ai).build();
+    private final Article articleAI3 = Article.builder().withFacultyName(ai).build();
+    private final Article articleGE = Article.builder().withFacultyName(ge).build();
+
+    private final Set<Article> articles = new HashSet<>(Arrays.asList(articleAI, articleAI2, articleAI3, articleGE));
+
+
+    @Test
+    void filter() {
+
+        this.facultyFilterName = ai;
+
+        Set<Article> expectedArticles = new HashSet<>(Arrays.asList(articleAI, articleAI2, articleAI3));
+
+        Set<Article> filteredArticles = new ArticleFilterModule(this.facultyFilterName, this.articles).filter();
+
+        Assertions.assertArrayEquals(expectedArticles.toArray(), filteredArticles.toArray());
+
+    }
+
+    @Test
+    void filterWithNotExistingFacultyName() {
+
+        this.facultyFilterName = "Architektur";
+
+        Set<Article> filteredArticles = new ArticleFilterModule(this.facultyFilterName, this.articles).filter();
+
+        Assertions.assertEquals(Collections.emptySet(),filteredArticles);
+
+    }
+}
