7d5fd9b9fdab1fcd4b154a43c5805fb8f06307e3
ch0914se <christof.seelisch@fh-erfurt.de>
2022-03-18 09:38:31 +0100

    Getters and Setters replaced with lombok @Getter and @Setter

--- persons-client/src/main/java/de/fherfurt/persons/client/transfer/objects/NewsPerson.java
+++ persons-client/src/main/java/de/fherfurt/persons/client/transfer/objects/NewsPerson.java
@@ -1,11 +1,16 @@
 package de.fherfurt.persons.client.transfer.objects;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Represents the Person information relevant for the News Module.
  * It is used to access Person Metadata to filter news articles with it.
  *
  * @author Christof Seelisch <christof.seelisch@fh-erfurt.de>
  */
+@Getter
+@Setter
 public class NewsPerson {
 
     private int id;
@@ -17,38 +22,6 @@ public class NewsPerson {
         this.id = id;
         this.name = name;
         this.mail = mail;
-        this.tel = tel;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public void setTel(String tel) {
         this.tel = tel;
     }
 }
