6a1f51aad3cf42f5def692eec806b1717c4307d4
MaximilianRoehr <maximilian.roehr@fh-erfurt.de>
2022-03-18 11:13:59 +0100

    just added the test Class

---
+++ news-service/src/test/java/de/fherfurt/news/articles/business/ArticleRequestHandlerTest.java
@@ -0,0 +1,26 @@
+package de.fherfurt.news.articles.business;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticleRequestHandlerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void handleRequest() {
+    }
+}
