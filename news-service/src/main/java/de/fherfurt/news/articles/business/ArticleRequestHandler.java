f3c732e24e28f3f19a8a6f3a40b69da25bdbd264
MaximilianRoehr <maximilian.roehr@fh-erfurt.de>
2022-03-18 11:06:19 +0100

    refactored ArticleRequestHandler
    -removed Singleton
    -added RequestType
    -missing: comments

--- news-service/src/main/java/de/fherfurt/news/articles/business/ArticleRequestHandler.java
+++ news-service/src/main/java/de/fherfurt/news/articles/business/ArticleRequestHandler.java
@@ -1,10 +1,16 @@
 package de.fherfurt.news.articles.business;
 
 
-import de.fherfurt.news.articles.business.modules.*;
+import de.fherfurt.news.articles.business.modules.ArticleFilterModule;
+import de.fherfurt.news.articles.business.modules.ArticleSearchModule;
+import de.fherfurt.news.articles.business.modules.ArticleSortModule;
+
+import de.fherfurt.news.articles.business.modules.entity.SortSettings;
 import de.fherfurt.news.articles.entity.Article;
-import de.fherfurt.news.core.persistance.dev.ArticleRepository;
+import de.fherfurt.news.articles.entity.PreviewRequest;
+import de.fherfurt.news.articles.entity.RequestType;
 import de.fherfurt.news.core.persistance.Repository;
+import lombok.AllArgsConstructor;
 
 import java.util.Collections;
 import java.util.List;
@@ -12,25 +18,14 @@ import java.util.Set;
 
 /**
  * TODO handle things like empty keywords and other cases
+ * all returned articles are always sorted, filtering and searching is optional
  *
- *  @author Maximilian Röhr <maximilian.roehr@fh-erfurt.de>
+ * @author Maximilian Röhr <maximilian.roehr@fh-erfurt.de>
  */
+@AllArgsConstructor
 public class ArticleRequestHandler {
 
-    private ArticleRequestHandler instance;
-    private Repository<Article> repository = new ArticleRepository();
-
-    public ArticleRequestHandler getInstance(){
-        if (this.instance == null){
-            initialize();
-        }
-        return this.instance;
-    }
-
-    private void initialize(){
-        this.repository = new ArticleRepository();
-        this.instance = new ArticleRequestHandler();
-    }
+    private final Repository<Article> repository;
 
     /**
      * find certain articles with given parameters {@link PreviewRequest}
@@ -38,20 +33,35 @@ public class ArticleRequestHandler {
      * @param request Request contains the facultyName,searchTerm and the sortSettings
      * @return
      */
-    public List<Article> handleRequest(PreviewRequest request) {
+    public List<Article> handleRequest(PreviewRequest request, RequestType requestType) {
         //fetching all current articles
         Set<Article> allCurrentArticles = repository.fetchAll();
 
-        //TODO das muss abgefangen werden
-        //guard clause to prevent unnecessary computing
+        //guard clause to return an empty list immediately
         if (allCurrentArticles.isEmpty()) return Collections.emptyList();
 
-        //fetching current articles and filtering them
-        Set<Article> filteredArticles = new ArticleFilterModule(request.facultyName, allCurrentArticles).filter();
-        //searching on filtered articles
-        Set<Article> searchedArticles = new ArticleSearchModule(request.searchTerm, filteredArticles).search();
-        //sorting those and returning them
-        return new ArticleSortModule(request.sortSettings,searchedArticles.stream().toList()).sort();
+        Set<Article> articles = Collections.emptySet();
+
+        //checks if it has to filter articles
+        if (requestType == RequestType.FILTER_SORT) {
+            //filtering fetched articles
+            articles = new ArticleFilterModule(request.facultyName, allCurrentArticles).filter();
+        }
+        //checks if it has to search through the filtered articles
+        else if (requestType == RequestType.FILTER_SEARCH_SORT){
+            //searching on filtered articles
+            articles = new ArticleSearchModule(request.searchTerm, articles).search();
+        }
+        return sortArticles(request.sortSettings, articles);
     }
 
+    /**
+     *
+     * @param sortSettings
+     * @param articles
+     * @return
+     */
+    private List<Article> sortArticles(SortSettings sortSettings, Set<Article> articles) {
+        return new ArticleSortModule(sortSettings, articles.stream().toList()).sort();
+    }
 }
