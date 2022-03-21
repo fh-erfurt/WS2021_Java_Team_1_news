9654157153c4a8eb387898ef30f1c31eb663e314
MaximilianRoehr <maximilian.roehr@fh-erfurt.de>
2022-03-21 12:16:37 +0100

    removed unnecessary import

--- appointments-client/src/test/java/de/fherfurt/appointments/client/DevAppointmentsServiceTest.java
+++ appointments-client/src/test/java/de/fherfurt/appointments/client/DevAppointmentsServiceTest.java
@@ -3,18 +3,16 @@ package de.fherfurt.appointments.client;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DevAppointmentsServiceTest {
 
     @Test
-    void shouldReturnAppointment1(){
+    void shouldReturnAppointment1() {
         DevAppointmentsService devAppointmentsService = new DevAppointmentsService();
         Assertions.assertEquals("appointment1", devAppointmentsService.getAppointmentById(1).get().getTitle());
     }
 
     @Test
-    void shouldReturn2(){
+    void shouldReturn2() {
         DevAppointmentsService devAppointmentsService = new DevAppointmentsService();
         Assertions.assertEquals(2, devAppointmentsService.getAppointmentByTitle("appointment2").get().getId());
     }
