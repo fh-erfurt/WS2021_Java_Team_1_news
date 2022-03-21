10bcfc07fbca166121e4ecfe6a457522fc15dc1f
MaximilianRoehr <maximilian.roehr@fh-erfurt.de>
2022-03-21 10:38:54 +0100

    replaced  constructor with builder and simplified assertion

--- news-service/src/test/java/de/fherfurt/news/articles/business/ArticleValidatorTest.java
+++ news-service/src/test/java/de/fherfurt/news/articles/business/ArticleValidatorTest.java
@@ -7,9 +7,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *  @author Christof Seelisch <christof.seelisch@fh-erfurt.de>
@@ -19,287 +18,288 @@ class ArticleValidatorTest {
     @Test
     void shouldReturnFalseEverythingEmpty(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder().build();
 
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnFalseEmptyTitle(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(4)
+                .withAppointmentId(1)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(4);
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
 
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnFalseEmptyContent(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(4)
+                .withAppointmentId(1)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(4);
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnFalseFalseResponsiblePersonIds(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3, 20))
+                .withAuthorId(4)
+                .withAppointmentId(1)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3, 20));
-        article.setAuthorId(4);
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnFalseFalseAuthorId(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(40)
+                .withAppointmentId(1)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(40);
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnFalseEmptyAuthorId(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAppointmentId(1)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnFalseFalseAppointmentId(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(4)
+                .withAppointmentId(10)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(4);
-        article.setAppointmentId(10);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnFalseFalseFacultyName(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(4)
+                .withAppointmentId(1)
+                .withFacultyName("faculty non existent")
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(4);
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty non existent");
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnFalseEmptyFacultyName(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(4)
+                .withAppointmentId(1)
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(4);
-        article.setAppointmentId(1);
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnFalseFalseDate(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(4)
+                .withAppointmentId(1)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(99999, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(4);
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(2023, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnFalseEmptyDate(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(4)
+                .withAppointmentId(1)
+                .withFacultyName("faculty1")
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(4);
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty1");
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnFalseEmptyLanguage(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(4)
+                .withAppointmentId(1)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(4);
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnFalseEmptyPriority(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(4)
+                .withAppointmentId(1)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(4);
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-
-        Assertions.assertEquals(false, validator.validateArticle(article));
+        Assertions.assertFalse(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnTrueEverythingCorrect(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(4)
+                .withAppointmentId(1)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(4);
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(true, validator.validateArticle(article));
+        Assertions.assertTrue(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnTrueEmptyPersons(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withAuthorId(4)
+                .withAppointmentId(1)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setAuthorId(4);
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(true, validator.validateArticle(article));
+        Assertions.assertTrue(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnTrueEmptyAppointment(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(4)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(2022, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(4);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(2022, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(true, validator.validateArticle(article));
+        Assertions.assertTrue(validator.validateArticle(article));
     }
 
     @Test
     void shouldReturnTrueDateLongAgo(){
         ArticleValidator validator = new ArticleValidator();
-        Article article = new Article();
+        Article article = Article.builder()
+                .withTitle("Example Title")
+                .withContent("Example Content")
+                .withResponsiblePersonIds(Set.of(1, 2, 3))
+                .withAuthorId(4)
+                .withAppointmentId(1)
+                .withFacultyName("faculty1")
+                .withDate(LocalDateTime.of(1900, Month.FEBRUARY, 14, 10, 0))
+                .withLanguage(Language.DE)
+                .withPriority(Priority.NORMAL)
+                .build();
 
-        article.setTitle("Example Title");
-        article.setContent("Example Content");
-        article.setResponsiblePersonIds(Set.of(1, 2, 3));
-        article.setAuthorId(4);
-        article.setAppointmentId(1);
-        article.setFacultyName("faculty1");
-        article.setDate(LocalDateTime.of(1900, 02, 14, 10, 0));
-        article.setLanguage(Language.DE);
-        article.setPriority(Priority.NORMAL);
-
-        Assertions.assertEquals(true, validator.validateArticle(article));
+        Assertions.assertTrue(validator.validateArticle(article));
     }
 }
