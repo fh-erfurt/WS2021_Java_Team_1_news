68c8c085b838bc17f7995ddcfd3b91ba5bfa9c0c
Pionec <dennis.rinck@fh-erfurt.de>
2022-03-23 15:20:56 +0100

    Added ArticlePreviewDto

---
+++ news-client/src/main/java/de/fherfurt/news/client/dto/ArticlePreviewDto.java
@@ -0,0 +1,53 @@
+package de.fherfurt.news.client.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class ArticlePreviewDto {
+
+    private int id;
+    private String title;
+    private int clicks;
+    private String facultyName;
+    private Set<String> keywords;
+    private LocalDateTime date;
+    private boolean wasModified;
+
+    @Builder(setterPrefix = "with")
+    public ArticlePreviewDto( int id,
+                       String title,
+                       String content,
+                       String facultyName,
+                       Set<String> keywords,
+                       LocalDateTime date){
+        this.id = id;
+        this.title = title;
+        this.facultyName = facultyName;
+        this.keywords = keywords;
+        this.date = date;
+
+    }
+
+    @Override
+    public boolean equals(Object object){
+        if (object == null || object.getClass() != this.getClass()) return false;
+
+        ArticlePreviewDto otherArticle = (ArticlePreviewDto) object;
+
+
+        //check all values if they are equal
+        return this.getId() == otherArticle.getId()
+                && this.title.equals(otherArticle.title) || (this.title == null && otherArticle.title == null)
+                && this.facultyName.equals(otherArticle.facultyName) || (this.facultyName == null && otherArticle.facultyName == null)
+                && this.keywords.equals(otherArticle.keywords) || (this.keywords == null && otherArticle.keywords == null)
+                && this.date.equals(otherArticle.date) || (this.date == null && otherArticle.date == null);
+    }
+}
